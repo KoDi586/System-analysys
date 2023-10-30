67b0fec88e8410f26de0a837e6b5bf2dff207fb0
hirof <hirofool502@gmail.com>
2023-10-30 11:46:09 +0300

    there button

--- src/main/java/com/example/graph_num_2/HelloController.java
+++ src/main/java/com/example/graph_num_2/HelloController.java
@@ -1,11 +1,28 @@
 package com.example.graph_num_2;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Canvas canvas;
+
+    public void initialize() {
+        // Вызывается при инициализации контроллера
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawPoints(gc);
+    }
+
+    private void drawPoints(GraphicsContext gc) {
+        // Здесь вы можете рисовать точки на холсте
+        gc.setFill(javafx.scene.paint.Color.RED);
+        gc.fillOval(50, 50, 5, 5); // Пример рисования красной точки в координатах (50, 50)
+    }
 
     @FXML
     protected void onHelloButtonClick() {
